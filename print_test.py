139a438ce3a09c49d8fbb6c6a2fc8a7ca31ca877
Akise17 <ahmad.fauzan1603@gmail.com>
2025-08-19 06:10:05 +0700

    update test print page

--- print_test.py
+++ print_test.py
@@ -1,25 +1,50 @@
 import sys
-from escpos.printer import File
+from escpos.printer import File, Serial
+import glob
+
+def find_printer_device():
+    usb_lp = sorted(glob.glob("/dev/usb/lp*"))
+    if usb_lp:
+        return ("file", usb_lp[0])
+
+    tty_usb = sorted(glob.glob("/dev/ttyUSB*"))
+    if tty_usb:
+        return ("serial", tty_usb[0])
+
+    return (None, None)
+
+def get_printer():
+  mode, device = find_printer_device()
+  if not device:
+      raise RuntimeError("No printer device found!")
+
+  print(f"[PRINTER] Using {mode.upper()} mode on {device}")
+
+  if mode == "file":
+      return File(device)
+  elif mode == "serial":
+      return Serial(
+          devfile=device,
+          baudrate=9600,
+          bytesize=8,
+          parity='N',
+          stopbits=1,
+          timeout=1.00
+      )
+
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: sudo python3 print_test.py <device_file>")
-        print("Example: sudo python3 print_test.py /dev/usb/lp0")
-        sys.exit(1)
+  try:
+    p = get_printer()
+    p.text("Hello, KP-628E!\n")
+    p.text("ESC/POS test print OK.\n")
+    p.text("--------------------------------\n")
+    p.cut()
 
-    port = sys.argv[1]
+    print("✅ Test print sent successfully")
 
-    try:
-        p = File(port)
-        p.text("Hello, KP-628E!\n")
-        p.text("ESC/POS test print OK.\n")
-        p.text("--------------------------------\n")
-        p.cut()
-
-        print(f"✅ Test print sent to {port}")
-
-    except Exception as e:
-        print(f"❌ Error: {e}")
+  except Exception as e:
+    print(f"❌ Error: {e}")
 
 if __name__ == "__main__":
     main()
